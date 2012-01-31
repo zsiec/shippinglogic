278fa330e705bc86d35a8ff4727292d0a4a5b29c
Thomas Symborski <thomas.symborski@gmail.com>
2012-01-31 14:32:29 -0500

    handle a just validate response

--- lib/shippinglogic/fedex/ship.rb
+++ lib/shippinglogic/fedex/ship.rb
@@ -221,17 +221,20 @@ module Shippinglogic
         # Making sense of the reponse and grabbing the information we need.
         def parse_response(response)
           details = response[:completed_shipment_detail]
-          rate = details[:shipment_rating][:shipment_rate_details].first[:total_net_charge]
-          package_details = details[:completed_package_details]
-          
-          shipment = Shipment.new
-          shipment.rate = BigDecimal.new(rate[:amount])
-          shipment.currency = rate[:currency]
-          shipment.delivery_date = Date.parse(details[:routing_detail][:delivery_date])
-          shipment.tracking_number = package_details[:tracking_id][:tracking_number]
-          shipment.label = package_details[:label][:parts][:image] && Base64.decode64(package_details[:label][:parts][:image])
-          shipment.barcode = package_details[:barcodes][:common2_d_barcode] && Base64.decode64(package_details[:barcodes][:common2_d_barcode])
-          shipment
+          if details
+            rate = details[:shipment_rating][:shipment_rate_details].first[:total_net_charge] if details.present?
+            package_details = details[:completed_package_details]
+            shipment = Shipment.new
+            shipment.rate = BigDecimal.new(rate[:amount])
+            shipment.currency = rate[:currency]
+            shipment.delivery_date = Date.parse(details[:routing_detail][:delivery_date])
+            shipment.tracking_number = package_details[:tracking_id][:tracking_number]
+            shipment.label = package_details[:label][:parts][:image] && Base64.decode64(package_details[:label][:parts][:image])
+            shipment.barcode = package_details[:barcodes][:common2_d_barcode] && Base64.decode64(package_details[:barcodes][:common2_d_barcode])
+            return shipment
+          else
+            return false
+          end
         end
     end
   end
